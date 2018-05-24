aa6c2902034bad9e39ab54d66bdaaac43eaacd4b
Vlad Stelea <vladstelea@gmail.com>
2018-05-24 16:44:27 -0400

    Working fragments and tab view

--- app/src/main/java/vlad/worchron/MainActivity.java
+++ app/src/main/java/vlad/worchron/MainActivity.java
@@ -1,6 +1,12 @@
 package vlad.worchron;
 
 import android.annotation.SuppressLint;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuBuilder;
@@ -9,13 +15,23 @@ import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.textColor));
+        viewPager = (ViewPager)findViewById(R.id.main_view_pager);
+        PagerAdapter pagerAdapter = new PageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.main_tab_layout);
+        tabs.setupWithViewPager(viewPager);
+        tabs.setTabTextColors(
+                getResources().getColor(R.color.textColor),
+                getResources().getColor(R.color.textColor)
+        );
     }
     @SuppressLint("RestrictedApi")
     @Override
@@ -25,5 +41,43 @@ public class MainActivity extends AppCompatActivity {
         //TODO Add the logo of the app
         //getSupportActionBar().setIcon(R.drawable.ic_launcher_foreground);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private class PageAdapter extends FragmentPagerAdapter{
+        private String [] tabTitles = {"Workouts", "Exercises"};
+        public PageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return the Fragment associated with a specified position.
+         *
+         * @param position
+         */
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new WorkoutsFragment();
+                case 1:
+                    return new ExercisesFragment();
+                    default:
+                        return null;
+
+            }
+        }
+
+        /**
+         * Return the number of views available.
+         */
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return tabTitles[position];
+        }
     }
 }
---
+++ app/src/main/java/vlad/worchron/WorkoutsFragment.java
@@ -0,0 +1,57 @@
+package vlad.worchron;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ */
+public class WorkoutsFragment extends Fragment {
+
+
+    public WorkoutsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment WorkoutsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WorkoutsFragment newInstance(String param1, String param2) {
+        WorkoutsFragment fragment = new WorkoutsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workouts, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
