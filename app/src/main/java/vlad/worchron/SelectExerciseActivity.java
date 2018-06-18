9495200b0bd72a46edb2577273c9ceb0bf547500
Vlad Stelea <vladstelea@gmail.com>
2018-06-18 17:48:12 -0400

    Created feature that allows new exercises to be created from the edit workout context

--- app/src/main/java/vlad/worchron/SelectExerciseActivity.java
+++ app/src/main/java/vlad/worchron/SelectExerciseActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.List;
 
+import vlad.backend.Algorithms.Sorters;
 import vlad.backend.Exercises.SelectableExercise;
 import vlad.backend.Exercises.SelectableTimedExercise;
 
@@ -19,15 +22,44 @@ public class SelectExerciseActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    //This button starts the create exercise activity
+    private ImageButton addExerciseButton;
+    private static final int CREATE_EXERCISE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_exercise);
         mExercises = MainActivity.ExerciseDAO.loadAllBackend();
+        setAddExerciseButtonInit();
         recyclerViewInit();
-    }
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            if(requestCode == CREATE_EXERCISE_REQUEST_CODE){
+                String accessCode = getString(R.string.create_exercise_result);
+                SelectableExercise newExercise = (SelectableExercise) data.getSerializableExtra(accessCode);
+                MainActivity.ExerciseDAO.saveBackend(newExercise,this);
+                Intent intent = new Intent();
+                String workoutKey = getString(R.string.select_exercise_exercise_key);
+                intent.putExtra(workoutKey,newExercise.createWorkoutVersion());
+                setResult(RESULT_OK, intent);
+                finish();
+
+            }
+        }
+    }
+    //<------------------Initializers-------------------------------------->
+
+    private void setAddExerciseButtonInit(){
+        addExerciseButton = findViewById(R.id.activity_select_exercise_add_button);
+        addExerciseButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, CreateExerciseActivity.class);
+            startActivityForResult(intent, CREATE_EXERCISE_REQUEST_CODE);
+        });
+    }
     private void recyclerViewInit(){
         //get the recycler view from the layout
         mRecyclerView = findViewById(R.id.activity_select_exercise_recycler_view);
